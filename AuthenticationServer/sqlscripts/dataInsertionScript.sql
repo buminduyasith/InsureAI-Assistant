80839c6bfd4887aecc5b9bf10be0d7b709167174
Bumindu Yasith <bumiduyasith@gmail.com>
2024-12-08 14:54:42 +0530

    insert admin and user roles into db

---
+++ AuthenticationServer/sqlscripts/dataInsertionScript.sql
@@ -0,0 +1,4 @@
+-- CREATE EXTENSION IF NOT EXISTS "uuid-ossp";  -- enable uuid generation
+
+INSERT INTO Roles (id, name)
+VALUES ('d069c5b7-52f6-434d-b04d-190ef34a55ce', 'Admin'), ("5bcd7efd-33dd-4f1e-8b8a-560aef9406de", 'User');
