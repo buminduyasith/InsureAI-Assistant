f6baf1f9596254742575afb8f25f3cbe7adf1187
Bumindu Yasith <bumiduyasith@gmail.com>
2024-12-08 16:45:06 +0530

    create dummy users

--- AuthenticationServer/sqlscripts/dataInsertionScript.sql
+++ AuthenticationServer/sqlscripts/dataInsertionScript.sql
@@ -2,3 +2,14 @@
 
 INSERT INTO Roles (id, name)
 VALUES ('d069c5b7-52f6-434d-b04d-190ef34a55ce', 'Admin'), ("5bcd7efd-33dd-4f1e-8b8a-560aef9406de", 'User');
+
+
+INSERT INTO users (id, first_name, last_name, email, password, created_at, updated_at)
+VALUES (uuid_generate_v4(), 'John', 'smith', 'john@gmail.com', 'hashPassword', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+-- Link admin user with the Admin role
+INSERT INTO userroles (user_id, role_id)
+VALUES ((SELECT id FROM users WHERE email = 'bumindu@gmail.com'), (SELECT id FROM roles WHERE name = 'Admin'));
+
+INSERT INTO userroles (user_id, role_id)
+VALUES ((SELECT id FROM users WHERE email = 'john@gmail.com'), (SELECT id FROM roles WHERE name = 'User'));
