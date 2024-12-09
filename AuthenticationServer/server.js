192744a84b086a8d2d3592bdb2bd8c259f589624
Bumindu Yasith <bumiduyasith@gmail.com>
2024-12-09 14:47:11 +0530

    add cors

--- AuthenticationServer/server.js
+++ AuthenticationServer/server.js
@@ -1,12 +1,24 @@
 const express = require("express");
+const cors = require("cors"); 
 const authController = require("./controllers/authController")
 const tokenController = require("./controllers/tokenController")
+const bcrypt = require("bcrypt");
 
 require("dotenv").config();
 
 const app = express();
 
+app.use(cors({
+    origin: "http://localhost:3000", // Allow requests from port 3000
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], // Allow these HTTP methods
+}));
+
 app.use(express.json());
+
+app.get("/", async (req, res) => {
+    var hash = await bcrypt.hashSync("egege@eE23", 10)
+    res.status(200).json({hash});
+})
 app.use("/auth", authController);
 app.use("/token", tokenController);
 
