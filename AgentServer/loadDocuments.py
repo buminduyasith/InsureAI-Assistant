6623ac8f5aeede7b7629669ecd2310b004313808
Bumindu Yasith <bumiduyasith@gmail.com>
2024-12-09 18:56:26 +0530

    insert all the document data to vector database

--- AgentServer/loadDocuments.py
+++ AgentServer/loadDocuments.py
@@ -1,52 +1,40 @@
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import PGVector
-import tqdm
+from langchain_openai import OpenAIEmbeddings
+from langchain_postgres import PGVector
+from langchain_postgres.vectorstores import PGVector
+import os
+from dotenv import load_dotenv
 
-
+load_dotenv()
 def loadData():
 
-    # Load the PDF
+    openai_api_key = os.getenv('OPENAI_API_KEY')
     loader = PyPDFLoader(file_path="test.pdf")
     documents = loader.load()
 
-    # Chunk the text
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,
-        chunk_overlap=200,
-        separators=["\n\n", "\n", ".", "  "]
-    )
-    docs = text_splitter.split_documents(documents)
-
+                chunk_size=1000,
+                chunk_overlap=200,
+                length_function=len,
+            )
+    
+    splitsdocs = text_splitter.split_documents(documents)
+    
     # Create embeddings
     embeddings = OpenAIEmbeddings(
-        openai_api_key="")
-
-    # Connect to Postgres Vector DB
-    pgvector = PGVector(
-        connection_string="postgresql://postgres:password123@localhost:5432/your_database",
-        embedding_function=embeddings
+        openai_api_key = openai_api_key
     )
 
-    # Add documents to the vector store with a progress bar
-    for i in tqdm.tqdm(range(0, len(docs), 100)):
-        batch = docs[i:i+100]
-        pgvector.add_documents(batch)
+    connection = "postgresql+psycopg://langchain:langchain@localhost:6024/langchain"  # Uses psycopg3!
+    collection_name = "my_docs"
 
+    vector_store = PGVector(
+        embeddings=embeddings,
+        collection_name=collection_name,
+        connection=connection,
+        use_jsonb=True,
+    )
 
-query = "What is the Coverage Amount?"
-
-embeddings = OpenAIEmbeddings(
-    openai_api_key="")
-
-pgvector = PGVector(
-    connection_string="postgresql://postgres:password123@localhost:5432/your_database",
-    embedding_function=embeddings
-)
-# Search the vector store
-docs = pgvector.similarity_search(query, k=1)
-
-# Print the relevant text
-for doc in docs:
-    print(doc.page_content)
+    vector_store.add_documents(splitsdocs)
+loadData()
