01762cf78d04a518f6c8a2af1764847803e29c1e
Bumindu Yasith <bumiduyasith@gmail.com>
2024-12-09 22:45:30 +0530

    use policy routes in the server

--- ClaimFlowAPI/server.js
+++ ClaimFlowAPI/server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const logger = require('./middlewares/logger');
 const claimsRoutes = require('./controllers/claimsController');
 const premiumRoutes = require('./controllers/premiumController');
+const policyRoutes = require('./controllers/policyController');
 
 require("dotenv").config();
 const app = express();
@@ -12,6 +13,7 @@ app.use(logger);
 
 app.use('/claims', claimsRoutes);
 app.use('/premium', premiumRoutes);
+app.use('/policy', policyRoutes);
 
 app.use((req, res) => {
     res.status(404).json({ error: 'Endpoint not found' });
